e3934d9b9da24cf1983329a77bd6fd074f9786d5
Reza Kurnia <ezakurnia50@gmail.com>
2022-04-18 00:18:25 +0700

    echo "# 10119189Latihan1" >> README.md git init git add README.md git commit -m "10119189Latihan1" git branch -M main git remote add origin https://github.com/itszakur/10119189Latihan1.git git push -u origin main

---
+++ app/src/main/java/com/example/a10119189latihan1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a10119189latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    ////17 April 2022, 10119189, Reza Kurnia, IF5
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
